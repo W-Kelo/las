4e3bfcf6434913234c86cff4cff2fc420d2ece76
W-Kelo <kontakt.rednaskela@gmail.com>
2026-05-30 21:32:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ const map = L.map('map', {
 L.control.zoom({ position: 'topright' }).addTo(map);
 
 const tiles = {
-    dark: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; Autorzy <a href="https://www.openstreetmap.org/">Open Street Map</a>', crossOrigin: true })
-};
+    dark: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; Autorzy <a href="https://www.openstreetmap.org/">Open Street Map</a>', crossOrigin: true }),
     light: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; Autorzy <a href="https://www.openstreetmap.org/">Open Street Map</a>', crossOrigin: true })
 };
 const satelliteLayer = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
